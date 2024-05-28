ea6976759748cae2ea1be683d555c65e573e8508
schwarzn <n.schwarz@wksgruppe.de>
2024-05-28 17:32:26 +0200

    Init

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import time                                                 # type: ignore
 
 
 def blink_func():
+    print("-> Blink")
     MyWS2812.do_blink()
 
 
@@ -73,7 +74,7 @@ if __name__ == "__main__":
         MyWS2812.setup_ws2812()
         ### Test ###
         #print("WS2812 -> Run self test")
-        MyWS2812.self_test()
+        #MyWS2812.self_test()
         #print("WS2812 -> Blink Test")
         #MyWS2812.do_blink_test()
         #print("WS2812 -> Dot-Test")
