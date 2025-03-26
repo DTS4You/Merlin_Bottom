90bd98c35492eeae99099f57e500e3a2e921163d
Norbert.Schwarz <info@datentechnikschwarz.de>
2025-03-26 21:10:11 +0100

    Init

--- libs/module_init.py
+++ libs/module_init.py
@@ -14,36 +14,60 @@ class Global_Module:
 
 class Global_WS2812:
 
-    numpix_1            = 128           # Anz. LEDs im 1. Stripe -> Alle LED Segmente
+    numpix_1            = 16            # Anz. LEDs im 1. Stripe -> Boden 1
+    numpix_2            = 16            # Anz. LEDs im 2. Stripe -> Boden 2
+    numpix_3            = 16            # Anz. LEDs im 3. Stripe -> Boden 3
+    numpix_4            = 16            # Anz. LEDs im 4. Stripe -> Boden 4
+    numpix_5            = 16            # Anz. LEDs im 5. Stripe -> Boden 5
+    numpix_6            = 256           # Anz. LEDs im 6. Stripe -> Spiegel, Laser, Empfänger
     #numpix_1           = 256           # Anz. LEDs im 1. Stripe -> Test !!!
 
-    seg_01_strip        = 0             #  1. Seg -> Stripe      # 0 -> Spiegel -> Ring
-    seg_01_start        = 0             #  1. Seg -> Start
-    seg_01_count        = 68            #  1. Seg -> Anzahl
+    #seg_01_strip        = 0             #  1. Seg -> Stripe      # 0 -> Spiegel -> Ring
+    #seg_01_start        = 0             #  1. Seg -> Start
+    #seg_01_count        = 68            #  1. Seg -> Anzahl
 
-    seg_02_strip        = 0             #  2. Seg -> Stripe      # 1 -> Laser Sender
-    seg_02_start        = 68            #  2. Seg -> Start
-    seg_02_count        = 23            #  2. Seg -> Anzahl
+    #seg_02_strip        = 0             #  2. Seg -> Stripe      # 1 -> Laser Sender
+    #seg_02_start        = 68            #  2. Seg -> Start
+    #seg_02_count        = 23            #  2. Seg -> Anzahl
 
-    seg_03_strip        = 0             #  3. Seg -> Stripe      # 2 -> Laser Empfänger
-    seg_03_start        = 91            #  3. Seg -> Start
-    seg_03_count        = 16            #  3. Seg -> Anzahl
+    #seg_03_strip        = 0             #  3. Seg -> Stripe      # 2 -> Laser Empfänger
+    #seg_03_start        = 91            #  3. Seg -> Start
+    #seg_03_count        = 16            #  3. Seg -> Anzahl
     
     #--------------------------------------------------------------------------
 
-    seg_04_strip        = 3             #  4. Seg -> Stripe      # Umlaufbahn neu innen
+    seg_01_strip        = 0             #  1. Seg -> Stripe      # 0 -> Boden 1
+    seg_01_start        = 0             #  1. Seg -> Start
+    seg_01_count        = 16            #  1. Seg -> Anzahl
+
+    seg_02_strip        = 1             #  2. Seg -> Stripe      # 1 -> Boden 2
+    seg_02_start        = 0             #  2. Seg -> Start
+    seg_02_count        = 16            #  2. Seg -> Anzahl
+
+    seg_03_strip        = 2             #  3. Seg -> Stripe      # 2 -> Boden 3
+    seg_03_start        = 0             #  3. Seg -> Start
+    seg_03_count        = 16            #  3. Seg -> Anzahl
+
+    seg_04_strip        = 3             #  4. Seg -> Stripe      # 3 -> Boden 4
     seg_04_start        = 0             #  4. Seg -> Start
-    seg_04_count        = 256           #  4. Seg -> Anzahl
+    seg_04_count        = 16            #  4. Seg -> Anzahl
 
-    seg_05_strip        = 4             #  5. Seg -> Stripe      # Umlaufbahn neu aussen
+    seg_05_strip        = 4             #  5. Seg -> Stripe      # 4 -> Boden 5
     seg_05_start        = 0             #  5. Seg -> Start
-    seg_05_count        = 256           #  5. Seg -> Anzahl
+    seg_05_count        = 16            #  5. Seg -> Anzahl
     
-    seg_06_strip        = 5             #  6. Seg -> Stripe      # Splitter-Strahl
+    seg_06_strip        = 5             #  6. Seg -> Stripe      # 5 -> Spiegel
     seg_06_start        = 0             #  6. Seg -> Start
-    seg_06_count        = 8             #  6. Seg -> Anzahl
+    seg_06_count        = 200           #  6. Seg -> Anzahl
     
+    seg_07_strip        = 6             #  7. Seg -> Stripe      # 5 -> Laser
+    seg_07_start        = 200           #  7. Seg -> Start
+    seg_07_count        = 16            #  7. Seg -> Anzahl
 
+    seg_08_strip        = 7             #  8. Seg -> Stripe      # 5 -> Empfänger
+    seg_08_start        = 216           #  8. Seg -> Start
+    seg_08_count        = 16            #  8. Seg -> Anzahl
+    
 # -----------------------------------------------------------------------------
 
     color_def           = (  0,  0,  2)
